c7091cb34cc962797d98c2d05fa2b0a7db8fe82d
tiruha <puhultu@yahoo.co.jp>
2016-04-20 10:58:02 +0900

    テーブル作成のSQLの追加とそれに対するchefの修正

--- chef/site-cookbooks/mysql/recipes/createdb_phpms.rb
+++ chef/site-cookbooks/mysql/recipes/createdb_phpms.rb
@@ -1,72 +1,165 @@
 # create database
 db_name = 'phpms'
-execute "createdb" do
-  command "/usr/bin/mysql -u root < #{Chef::Config[:file_cache_path]}/createdb.sql"
+create_db_file = 'create_db.sql'
+execute "create_db" do
+  command "/usr/bin/mysql -u root < #{Chef::Config[:file_cache_path]}/#{create_db_file}"
   action :nothing
   not_if "/usr/bin/mysql -u root -D #{db_name}"
 end
 
-template "#{Chef::Config[:file_cache_path]}/createdb.sql" do
+template "#{Chef::Config[:file_cache_path]}/#{create_db_file}" do
   owner 'root'
   group 'root'
   mode 644
-  source 'createdb.sql.erb'
+  source "#{create_db_file}.erb"
   variables({
     :db_name => db_name,
     })
-  notifies :run, 'execute[createdb]', :immediately
+  notifies :run, 'execute[create_db]', :immediately
 end
 
 # create database user
 user_name = 'phpms'
 user_password = 'phpms'
-execute 'createuser' do
-  command "/usr/bin/mysql -u root < #{Chef::Config[:file_cache_path]}/createuser.sql"
+create_user_file = 'create_user.sql'
+execute 'create_user' do
+  command "/usr/bin/mysql -u root < #{Chef::Config[:file_cache_path]}/#{create_user_file}"
   action :nothing
   not_if "/usr/bin/mysql -u #{user_name} -p#{user_password} -D #{db_name}"
 end
 
-template "#{Chef::Config[:file_cache_path]}/createuser.sql" do
+template "#{Chef::Config[:file_cache_path]}/#{create_user_file}" do
   owner 'root'
   group 'root'
   mode 644
-  source 'createuser.sql.erb'
+  source "#{create_user_file}.erb"
   variables({
       :db_name => db_name,
       :username => user_name,
       :password => user_password,
     })
-  notifies :run, 'execute[createuser]', :immediately
+  notifies :run, 'execute[create_user]', :immediately
 end
 
 # create schema
 schema_file = 'create_schema_phpms.sql'
 execute "create_schema_phpms" do
-  command "/usr/bin/mysql -u root #{db_name} < #{Chef::Config[:file_cache_path]}/create_schema_phpms.sql"
+  command "/usr/bin/mysql -u root #{db_name} < #{Chef::Config[:file_cache_path]}/#{schema_file}"
   action :nothing
   not_if "/usr/bin/mysql -u #{user_name} -p#{user_password} -D #{db_name}  -e 'show tables' | wc -l | xargs expr 1 /"
 end
 
-template "#{Chef::Config[:file_cache_path]}/create_schema_phpms.sql" do
+template "#{Chef::Config[:file_cache_path]}/#{schema_file}" do
   owner 'root'
   group 'root'
   mode 644
-  source 'create_schema_phpms.sql.erb'
+  source "#{schema_file}.erb"
   notifies :run, 'execute[create_schema_phpms]', :immediately
 end
 
-schema_file = 'create_schema_login_user.sql'
-execute "create_schema_login_user" do
-  command "/usr/bin/mysql -u root #{db_name} < #{Chef::Config[:file_cache_path]}/create_schema_login_user.sql"
+# create table
+table_file = 'create_table_login_user.sql'
+execute "create_table_login_user" do
+  command "/usr/bin/mysql -u root #{db_name} < #{Chef::Config[:file_cache_path]}/#{table_file}"
   action :nothing
   not_if "/usr/bin/mysql -u #{user_name} -p#{user_password} -D #{db_name}  -e 'show tables' | wc -l | xargs expr 1 /"
 end
 
-template "#{Chef::Config[:file_cache_path]}/create_schema_login_user.sql" do
+template "#{Chef::Config[:file_cache_path]}/#{table_file}" do
   owner 'root'
   group 'root'
   mode 644
-  source 'create_schema_login_user.sql.erb'
-  notifies :run, 'execute[create_schema_login_user]', :immediately
+  source "#{table_file}.erb"
+  notifies :run, 'execute[create_table_login_user]', :immediately
+end
+
+table_file = 'create_table_url.sql'
+execute "create_table_url" do
+  command "/usr/bin/mysql -u root #{db_name} < #{Chef::Config[:file_cache_path]}/#{table_file}"
+  action :nothing
+  not_if "/usr/bin/mysql -u #{user_name} -p#{user_password} -D #{db_name}  -e 'show tables' | wc -l | xargs expr 1 /"
+end
+
+template "#{Chef::Config[:file_cache_path]}/#{table_file}" do
+  owner 'root'
+  group 'root'
+  mode 644
+  source "#{table_file}.erb"
+  notifies :run, 'execute[create_table_url]', :immediately
+end
+
+table_file = 'create_table_synonym.sql'
+execute "create_table_synonym" do
+  command "/usr/bin/mysql -u root #{db_name} < #{Chef::Config[:file_cache_path]}/#{table_file}"
+  action :nothing
+  not_if "/usr/bin/mysql -u #{user_name} -p#{user_password} -D #{db_name}  -e 'show tables' | wc -l | xargs expr 1 /"
+end
+
+template "#{Chef::Config[:file_cache_path]}/#{table_file}" do
+  owner 'root'
+  group 'root'
+  mode 644
+  source "#{table_file}.erb"
+  notifies :run, 'execute[create_table_synonym]', :immediately
+end
+
+table_file = 'create_table_url_group.sql'
+execute "create_table_url_group" do
+  command "/usr/bin/mysql -u root #{db_name} < #{Chef::Config[:file_cache_path]}/#{table_file}"
+  action :nothing
+  not_if "/usr/bin/mysql -u #{user_name} -p#{user_password} -D #{db_name}  -e 'show tables' | wc -l | xargs expr 1 /"
+end
+
+template "#{Chef::Config[:file_cache_path]}/#{table_file}" do
+  owner 'root'
+  group 'root'
+  mode 644
+  source "#{table_file}.erb"
+  notifies :run, 'execute[create_table_url_group]', :immediately
+end
+
+table_file = 'create_table_similar_url.sql'
+execute "create_table_similar_url" do
+  command "/usr/bin/mysql -u root #{db_name} < #{Chef::Config[:file_cache_path]}/#{table_file}"
+  action :nothing
+  not_if "/usr/bin/mysql -u #{user_name} -p#{user_password} -D #{db_name}  -e 'show tables' | wc -l | xargs expr 1 /"
+end
+
+template "#{Chef::Config[:file_cache_path]}/#{table_file}" do
+  owner 'root'
+  group 'root'
+  mode 644
+  source "#{table_file}.erb"
+  notifies :run, 'execute[create_table_similar_url]', :immediately
+end
+
+table_file = 'create_table_parameters.sql'
+execute "create_table_parameters" do
+  command "/usr/bin/mysql -u root #{db_name} < #{Chef::Config[:file_cache_path]}/#{table_file}"
+  action :nothing
+  not_if "/usr/bin/mysql -u #{user_name} -p#{user_password} -D #{db_name}  -e 'show tables' | wc -l | xargs expr 1 /"
+end
+
+template "#{Chef::Config[:file_cache_path]}/#{table_file}" do
+  owner 'root'
+  group 'root'
+  mode 644
+  source "#{table_file}.erb"
+  notifies :run, 'execute[create_table_parameters]', :immediately
+end
+
+table_file = 'create_table_rss_url.sql'
+execute "create_table_rss_url" do
+  command "/usr/bin/mysql -u root #{db_name} < #{Chef::Config[:file_cache_path]}/#{table_file}"
+  action :nothing
+  not_if "/usr/bin/mysql -u #{user_name} -p#{user_password} -D #{db_name}  -e 'show tables' | wc -l | xargs expr 1 /"
+end
+
+template "#{Chef::Config[:file_cache_path]}/#{table_file}" do
+  owner 'root'
+  group 'root'
+  mode 644
+  source "#{table_file}.erb"
+  notifies :run, 'execute[create_table_rss_url]', :immediately
 end
 
